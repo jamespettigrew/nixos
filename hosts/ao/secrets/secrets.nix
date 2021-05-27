161529265546186538b40fe623bf3105bbd2bf27
James Pettigrew <james@innovum.com.au>
2021-05-27 06:22:17 -0400

    Remove hlissner's host configs.

--- hosts/ao/secrets/secrets.nix
+++
@@ -1,5 +0,0 @@
-let key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINd+4ijlEHI98NLpgP/El73BmpBUfzka9ovdt8FoaVAG hlissner@home";
-in {
-  "bitwarden-smtp-env.age".publicKeys = [key];
-  "gitea-smtp.age".publicKeys = [key];
-}
