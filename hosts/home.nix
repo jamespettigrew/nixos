1dc79402eddae6dfd4659c99ed4668273a8e0fe7
James Pettigrew <james@innovum.com.au>
2021-05-28 22:39:33 -0400

    Update lat/long and timezone values.

--- hosts/home.nix
+++ hosts/home.nix
@@ -2,16 +2,13 @@
 
 with lib;
 {
-  ## Location config -- since Toronto is my 127.0.0.1
-  time.timeZone = mkDefault "America/Toronto";
+  ## Location config -- since Melbourne is my 127.0.0.1
+  time.timeZone = mkDefault "Australia/Melbourne";
   i18n.defaultLocale = mkDefault "en_US.UTF-8";
   # For redshift, mainly
-  location = (if config.time.timeZone == "America/Toronto" then {
-    latitude = 43.70011;
-    longitude = -79.4163;
-  } else if config.time.timeZone == "Europe/Copenhagen" then {
-    latitude = 55.88;
-    longitude = 12.5;
+  location = (if config.time.timeZone == "Australia/Melbourne" then {
+    latitude = -37.8136;
+    longitude = 144.9631;
   } else {});
 
   # So thw bitwarden CLI knows where to find my server.
