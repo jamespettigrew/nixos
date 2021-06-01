8a2ab7137e10d27a67da175ee2c3560bea72e847
James Pettigrew <james@innovum.com.au>
2021-06-01 19:31:39 +1000

    Switch to Zen kernel as this is a desktop system.

--- default.nix
+++ default.nix
@@ -47,7 +47,7 @@ with lib.my;
 
   # Use the latest kernel
   boot = {
-    kernelPackages = mkDefault pkgs.linuxPackages_5_10;
+    kernelPackages = mkDefault pkgs.linuxPackages_zen;
     loader = {
       efi.canTouchEfiVariables = mkDefault true;
       systemd-boot.configurationLimit = 10;
