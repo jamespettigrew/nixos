2d5ea6908b594a735b3dbb98d19ee4ebfc8c7671
James Pettigrew <james@innovum.com.au>
2021-07-04 18:29:12 +1000

    Add packages for Zoom virtual backgrounds.

--- modules/desktop/apps/zoom.nix
+++ modules/desktop/apps/zoom.nix
@@ -9,7 +9,21 @@ in {
   };
 
   config = mkIf cfg.enable {
+    boot.extraModulePackages = with unstable; [
+      config.boot.kernelPackages.v4l2loopback
+    ];
+
+    # Register a v4l2loopback device at boot
+    boot.kernelModules = [
+      "v4l2loopback"
+    ];
+
+    boot.extraModprobeConfig = ''
+      options v4l2loopback exclusive_caps=1 video_nr=20 card_label=v4l2loopback
+    '';
+
     user.packages = with pkgs; [
+      v4l-utils
       zoom-us
     ];
   };
